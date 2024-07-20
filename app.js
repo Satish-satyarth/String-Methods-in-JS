979c0b0b47f10b9acf46eae25d9e35e6adeafb8e
satish-satyarth <satishsatyarth2002@gmail.com>
2024-07-20 14:25:09 +0530

    New updated

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const name = "satish_satyarth";
 console.log(name);
+
+console.log(name.length);
+
+console.log(name.char.At(5));
+
+console.log(name.toUppercase());
+
+//The charCodeAt() method returns the Unicode of the character at a specified index (position) in a string.
+console.log(name.charCodeAt('s'));
+
+console.log(name.toLowercase());
+
+// Trim -> The trim() method removes whitespace from both sides of a string.
+
+let UserName = "   Satishsatyarth2002@gmail.com   ";
+console.log(UserName.trim());
