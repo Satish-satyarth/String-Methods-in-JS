d1c9b1c968ec490b5a5b1f250f8c4452b1ab1180
satish-satyarth <satishsatyarth2002@gmail.com>
2024-07-20 14:54:18 +0530

    latest Updateds

--- app.js
+++ app.js
@@ -14,5 +14,29 @@ console.log(name.toLowercase());
 
 // Trim -> The trim() method removes whitespace from both sides of a string.
 
-let UserName = "   Satishsatyarth2002@gmail.com   ";
-console.log(UserName.trim());
+let userName = "   Satishsatyarth2002@gmail.com   ";
+console.log(userName.trim());
+
+// The substring() method extracts characters from start to end (exclusive).
+console.log(userName.substring(0,10));
+
+
+// The slice() method extracts a part of a string.
+console.log(userName.slice(-5,10));
+
+//  Replace -> replace(pattern, replacement)
+// ReplaceAll -> 
+console.log(userName.replace('2002', 'yadav'));
+
+/*
+The includes() method returns true if a string contains a specified string.
+
+Otherwise it returns false.
+
+The includes() method is case sensitive.
+*/
+console.log(userName.includes("satyarth"));
+
+
+let Myadd = "vill Barajor post barajor ps jhajha";
+console.log(Myadd.split(" "));
